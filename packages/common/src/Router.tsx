a770a66990671a16aa30e9ae7a4aac8c2737eb7c
Leo Kim <ghleokim@gmail.com>
2020-01-22 11:48:43 +0900

    S02P12A102-18 Add Banner Example

--- packages/common/src/Router.tsx
+++ packages/common/src/Router.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { View, Text, Button } from "react-native"
-import { TestPage } from './modules/testpage';
+import { MainPage } from './modules/MainPage';
 import { SecondTestPage } from './modules/secondtestpage';
 
 export const Router: React.FC = () => {
   const [page, setPage] = useState(0);
 
   const rendering = page === 0 ? (
-    <TestPage page={page} setPage={setPage} />
+    <MainPage page={page} setPage={setPage} />
   ) : (
       <SecondTestPage page={page} setPage={setPage} />
     )
