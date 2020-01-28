f7447aae52b55c5557b8f8f2c9c4274f2706e019
Leo Kim <ghleokim@gmail.com>
2020-01-28 18:59:23 +0900

    make routelist

--- packages/common/src/modules/RouteMap.tsx
+++ packages/common/src/modules/RouteMap.tsx
@@ -4,28 +4,17 @@ import { StyleSheet, Text, View, Button, Dimensions, TouchableOpacity } from 're
 import { mainStoreContext } from '../store/MainStore';
 import { observer } from 'mobx-react-lite';
 import { SearchList } from '../components/result/SearchList';
+import { RouteList } from './RouteList';
 
 export const RouteMap: React.FC = observer(() => {
   const mainStore = useContext(mainStoreContext);
   const currentPage = mainStore.currentPage
 
-  const getCoordinate = () => {
-    const cur = navigator.geolocation.getCurrentPosition(
-      function (position) {
-        console.log(position)
-      },
-      function(positionError) {
-        console.log('error', positionError)
-      }
-      )
-  }
-
   return (
     <View>
-      <SearchList />
       <Text style={styles.sectionTitle}>{currentPage}</Text>
+      <RouteList />
       {/* put content here */}
-      <TouchableOpacity onPress={getCoordinate}><Text>test</Text></TouchableOpacity>
       <Text style={styles.sectionDescription}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consectetur nisl gravida elit egestas, vitae egestas nisi luctus. Suspendisse commodo, tortor vitae tincidunt rhoncus, nisl tellus dictum metus, sit amet fringilla eros ligula et velit. Mauris tempus diam turpis, at rutrum dui semper nec. Nunc viverra, lectus at pharetra accumsan, metus risus varius libero, ac viverra ipsum quam nec dolor. Vivamus diam sem, ornare sit amet justo at, fermentum rutrum nunc. Vestibulum suscipit congue elit, id laoreet massa eleifend quis. Phasellus risus elit, vulputate sit amet ex vel, faucibus imperdiet eros. Suspendisse est lorem, finibus eget facilisis tempus, sagittis vel metus. Aenean pharetra condimentum magna in congue. Cras aliquet vestibulum congue. Nullam pellentesque vitae diam id malesuada. Nunc nec dui neque. Praesent ut metus eu arcu mattis semper sit amet sed tellus. Morbi eu viverra lectus. Cras purus dui, sodales vel dolor vel, feugiat ultricies sem.
         Donec consectetur eu tellus sit amet laoreet. Mauris ipsum justo, tincidunt in nibh at, imperdiet finibus enim. Ut sit amet dui purus. Aenean at mi a lacus pellentesque molestie sed sit amet nisl. Donec sit amet turpis a erat lacinia placerat non at leo. Fusce bibendum hendrerit neque vel congue. Duis suscipit vitae dolor in sollicitudin. Proin at sagittis mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis fringilla dui faucibus sapien euismod, et eleifend quam tincidunt. Proin ultrices elit ac ornare viverra. Mauris volutpat porttitor ornare. Quisque eget nulla massa. Curabitur convallis purus vitae odio porta luctus eu aliquet augue. Nulla facilisis tristique malesuada.
