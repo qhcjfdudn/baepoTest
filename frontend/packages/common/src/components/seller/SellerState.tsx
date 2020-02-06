340e9ada21100c2896834d49c8053b4dca45064f
SteelFox <jhjhyjin@naver.com>
2020-02-06 10:38:47 +0900

    create inner Seller Map

--- frontend/packages/common/src/components/seller/SellerState.tsx
+++ frontend/packages/common/src/components/seller/SellerState.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Button,
 } from "react-native";
+import { SellerMaps } from '../map/sellerMaps';
 
 export default () => {
 
-  const a = () => {}
-  const b = () => {}
-  const c = () => {}
+  const [data, setData] = useState({open: false, prepairing: false, closed: true});
 
+  const open = (v) => {
+    if(v == 0) setData({open: true, prepairing: false, closed: false});
+    else if(v == 1) setData({open: false, prepairing: true, closed: false});
+    else if(v == 2) setData({open: false, prepairing: false, closed: true});
+  }
+  
     return (
       <View>
-        <Button title="영업중" onPress={() => a()}></Button>
-        <Button title="준비중" onPress={() => b()}></Button>
-        <Button title="Closed" onPress={() => c()}></Button>
+        { (data.open == true || data.prepairing == true) && <SellerMaps/> }
+        { data.closed == true &&
+          <View>
+            <Button title="영업중" onPress={() => open(0)}></Button>
+            <Button title="준비중" onPress={() => open(1)}></Button>
+            <Button title="Closed" onPress={() => open(2)}></Button>
+          </View>
+        }
       </View>
     ) 
 }
