ea6e126684cdf8aa1a809883e7493a0a1aec632d
Geonho Kim / Leo Kim <gh.leokim@gmail.com>
2020-02-07 14:59:24 +0900

    fix login

--- frontend/packages/common/src/components/seller/SellerMain.tsx
+++ frontend/packages/common/src/components/seller/SellerMain.tsx
@@ -61,8 +61,10 @@ export default () => {
 
   const mainStore = useContext(mainStoreContext)
 
+  const myTruckId = localStorage.getItem('truckId')
+
   useEffect(() => {
-    axios.get(`/trucks/${mainStore.sellerTruckId === undefined ? '1' : mainStore.sellerTruckId}`)
+    axios.get(`/trucks/${myTruckId === undefined ? '1' : myTruckId}`)
       .then((res) => {
         setData(res.data.result);
         setEditText(res.data.result);
